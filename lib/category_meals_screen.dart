e2515b91c5311a1a7320fab31a3371a591a5296c
Jimbo Cortes <jimbo.cortes@gmail.com>
2019-08-31 21:41:07 +0800

    - Pass data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle)
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(this.categoryId),
       ),
       body: Center(
-        child: Text('The Recipes for the Category'),
+        child: Text(this.categoryTitle),
       ),
     );
   }
